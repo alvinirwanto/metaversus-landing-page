df025f81d9e910594c41bf8e8e4d47580d426d1d
alvinirwanto <irwantoalvin@gmail.com>
2022-12-01 17:38:52 +0700

    finish all the sections

--- components/InsightCard.jsx
+++ components/InsightCard.jsx
@@ -1,9 +1,39 @@
 'use client';
 
-const InsightCard = () => (
-  <div>
-    Insight Card
-  </div>
+import { motion } from "framer-motion";
+import { fadeIn } from "../utils/motion";
+import Image from "next/image";
+
+const InsightCard = ({ imgUrl, title, subtitle, index }) => (
+    <motion.div
+        variants={fadeIn('up', 'spring', index * 0.5, 1)}
+        className='flex md:flex-row flex-col gap-4'
+    >
+        <Image
+            src={imgUrl}
+            alt='planet-01'
+            width={1500}
+            height={1500}
+            className="md:w-[270px] w-full h-[250px] rounded-[32px] object-cover"
+        />
+
+        <div className="w-full flex justify-between items-center">
+            <div className="flex-1 md:ml-[62px] flex flex-col max-w-[650px]">
+                <h4 className="font-normal lg:text-[32px] text-[26px] text-white">{title}</h4>
+                <p className="mt-[16px] font-normal lg:text-[20px] text-[14px] text-secondary-white">{subtitle}</p>
+            </div>
+
+            <div className="lg:flex hidden items-center justify-center w-[100px] aspect-square rounded-full bg-transparent border-[1px] border-white">
+                <Image
+                    src='/arrow.svg'
+                    alt="arrow"
+                    width={1000}
+                    height={1000}
+                    className="w-[40%] aspect-square object-contain"
+                />
+            </div>
+        </div>
+    </motion.div>
 );
 
 export default InsightCard;
--- sections/World.jsx
+++ sections/World.jsx
@@ -29,17 +29,20 @@ const World = () => (
 
             <motion.div
                 variants={fadeIn('up', 'tween', 0.3, 1)}
-                className='relative mt-[68px] flex w-full h-[550px]'
+                className='relative mt-[68px] flex w-full h-[300px] md:h-[550px] mb-[5rem]'
             >
                 <Image
                     src='/map.png'
                     alt='map'
                     width={2000}
                     height={2000}
-                    className='w-full h-full object-cover'
+                    className='w-full h-full object-contain'
                 />
 
-                <div className='absolute bottom-20 right-20 w-[70px] h-[70px] p-[6px] rounded-full bg-[#5d6680]'>
+                <div className='absolute
+                w-[40px] p-[3px] bottom-[5rem] right-[1rem]
+                md:bottom-[12rem] md:right-[4rem] md:w-[50px]
+                xl:bottom-20 xl:right-20 xl:w-[70px] aspect-square md:p-[6px] rounded-full bg-[#5d6680]'>
                     <Image
                         src='/people-01.png'
                         alt='people'
@@ -49,7 +52,9 @@ const World = () => (
                     />
                 </div>
 
-                <div className='absolute top-10 left-20 w-[70px] h-[70px] p-[6px] rounded-full bg-[#5d6680]'>
+                <div className='absolute 
+                w-[40px] p-[3px] top-[10rem] left-[3rem]
+                md:top-[12rem] md:left-20 md:w-[50px] xl:w-[70px] aspect-square md:p-[6px] rounded-full bg-[#5d6680]'>
                     <Image
                         src='/people-02.png'
                         alt='people'
@@ -59,7 +64,7 @@ const World = () => (
                     />
                 </div>
 
-                <div className='absolute top-1/2 left-[45%] w-[70px] h-[70px] p-[6px] rounded-full bg-[#5d6680]'>
+                <div className='absolute top-1/2 left-[45%] md:w-[50px] xl:w-[70px] aspect-square p-[6px] rounded-full bg-[#5d6680]'>
                     <Image
                         src='/people-03.png'
                         alt='people'
@@ -69,7 +74,9 @@ const World = () => (
                     />
                 </div>
 
-                <div className='absolute -top-[90px] right-[300px] w-[400px] h-auto'>
+                <div className='absolute 
+                xl:-top-[90px] xl:right-[300px] xl:w-[400px] 
+                md:top-[5rem] md:right-[5rem] md:w-[250px] h-auto'>
                     <Image
                         src='/people-group-01.svg'
                         alt='people'
@@ -79,7 +86,10 @@ const World = () => (
                     />
                 </div>
 
-                <div className='absolute top-[30%] left-[100px] w-[350px] h-auto'>
+                <div className='absolute 
+                top-0 right-0
+                md:bottom-[7rem] md:w-[200px]
+                xl:top-[30%] xl:left-[100px] xl:w-[350px] h-auto'>
                     <Image
                         src='/people-group-02.svg'
                         alt='people'
